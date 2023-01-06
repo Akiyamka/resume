c4c4696d66cf6147d53a524ee8e37d3be4a6cc56
Alexandr Dubinin <akiyamka@gmail.com>
2023-01-06 16:04:07 +0100

    add position parameter

--- src/common/utils/toJSONResumeFormat.ts
+++ src/common/utils/toJSONResumeFormat.ts
@@ -1,6 +1,7 @@
 import { Resume } from '~types';
 import { ResumeSchema as JSONResume } from '@supastuff/json-resume-types';
 import config from '~config';
+import { omitUndefinedProps } from './omitUndefinedProps';
 
 /* https://jsonresume.org/ */
 export function toJSONResumeFormat(r: Resume): JSONResume {
@@ -9,24 +10,32 @@ export function toJSONResumeFormat(r: Resume): JSONResume {
       name: r.name,
       label: r.job_title,
       image: r.photo,
-      email: r.contacts.find(c => c.type === 'email')?.value ?? 'hidden',
+      email: r.contacts.find((c) => c.type === 'email')?.value ?? 'hidden',
       phone: 'hidden',
       url: config.data_dir,
-      summary: "",
-      location: {
+      summary: '',
+      location: omitUndefinedProps({
         postalCode: r.location.postalCode,
         city: r.location.city,
         address: r.location.address,
         countryCode: r.location.country,
-      },
-      profiles: r.contacts.filter(c => c.type === 'social').map(c => ({
-        network: c.label,
-        url: c.link,
-        username: c.value
-      })),
-      work: r.experience.periods.map(e => ({
-
-      }))
-    }
-  }
+      }),
+      profiles: r.contacts
+        .filter((c) => c.type === 'social')
+        .map((c) => ({
+          network: c.label,
+          url: c.link,
+          username: c.value,
+        })),
+      work: r.experience.periods.map((e) => (omitUndefinedProps({
+        name: e.company_name,
+        position: e.position,
+        url: e.homepage,
+        startDate: e.date_start,
+        endDate: e.date_end,
+        summary: e.summary,
+        highlights: e.responsibilities,
+      }))),
+    },
+  };
 }
