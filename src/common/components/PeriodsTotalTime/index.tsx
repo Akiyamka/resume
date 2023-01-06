d21d7e0b3e9b0077d714bc6ca44615812cb779e7
Eugene Domanovich <89357747+Dompower25@users.noreply.github.com>
2023-01-06 18:05:31 +0300

    Tooltips for years (from to) (#3)
    * adding years difference
    
    * update readme
    
    * Update src/common/components/PeriodsTotalTime/index.tsx
    
    Co-authored-by: Alexander CherryTea <Akiyamka@gmail.com>
    
    Co-authored-by: Alexander CherryTea <Akiyamka@gmail.com>

--- src/common/components/PeriodsTotalTime/index.tsx
+++ src/common/components/PeriodsTotalTime/index.tsx
@@ -10,6 +10,12 @@ const nowInUTC = () => new Date(Date.now()).toUTCString();
 const getDiff = (utcStart: string, utcEnd?: string) =>
   formatDistanceStrict(new Date(utcStart), new Date(utcEnd ?? nowInUTC()));
 
+const getYears = (utcStart: string, utcEnd?: string) => {
+  const start = new Date(utcStart).getFullYear();
+  const end = new Date(utcEnd ?? nowInUTC()).getFullYear();
+  return `${start} - ${end}`;
+};
+
 const getTotalExp = (sortedPeriods: Period[]): string | null => {
   const firstExp = sortedPeriods.at(-1)?.date_start;
   if (firstExp) {
@@ -23,7 +29,17 @@ export function PeriodsTotalTime({ periods }: { periods: Period[] }) {
   return totalExp ? <div>{totalExp}</div> : null;
 }
 
-export function TimeDistanceInWords(props: { utcStart: string; utcEnd?: string; class?: string }) {
+export function TimeDistanceInWords(props: {
+  utcStart: string;
+  utcEnd?: string;
+  class?: string;
+}) {
   const distance = getDiff(props.utcStart, props.utcEnd);
-  return distance ? <div class={props.class}>{distance}</div> : null;
+  const distanceYear = getYear(props.utcStart, props.utcEnd);
+  return distance ? (
+    <div title={distanceYear}>
+      {distance}
+      <sup>?</sup>
+    </div>
+  ) : null;
 }
